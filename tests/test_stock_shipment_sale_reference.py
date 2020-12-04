c69f7ac1e0ba23d10ea19bec1db119d9ceda89d5
Albert Cervera i Areny <albert@nan-tic.com>
2020-12-05 00:48:50 +0100

    Pyflakes fixes.

--- tests/test_stock_shipment_sale_reference.py
+++ tests/test_stock_shipment_sale_reference.py
@@ -2,11 +2,8 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
 import unittest
-import doctest
 import trytond.tests.test_tryton
 from trytond.tests.test_tryton import ModuleTestCase
-from trytond.tests.test_tryton import doctest_setup, doctest_teardown
-from trytond.tests.test_tryton import doctest_checker
 
 
 class StockSaleReferencesTestCase(ModuleTestCase):
